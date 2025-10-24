5da434f961378e45a024640f3dd20e6874ce3428
Yuranyy14 <cardenasuribeyurany@gmail.com>
2025-10-24 17:14:36 -0500

    Escenario interactivo sin terminar

--- 02_Java_escenario_interactivo/app.js
+++ 02_Java_escenario_interactivo/app.js
@@ -1,3 +1,5 @@
+// Escenario 1
+
 // 1. captura de variables
 const corazon = document.querySelectorAll(".cora")
 const tablero = document.querySelector(".contadorCorazon")
@@ -13,6 +15,43 @@ corazon.forEach( item => {
         item.classList.add("saltar")
         counter++
         tablero.textContent = counter
+    })
+
+    })
+
+
+// 3. Eventos 
+
+
+//Escenario 2
+// 1. captura de variables
+const vaca = document.querySelectorAll(".vaca")
+const cerdo = document.querySelectorAll(".cerdo")
+const tableroVca = document.querySelector(".countVaca")
+const tableroCrdo = document.querySelector(".countPig")
+let counterV = 0 
+
+console.log(vaca, tableroVca, cerdo, tableroCrdo)
+
+// 2. funciones 
+vaca.forEach( item => {
+    
+    item.addEventListener("click", () => {
+        item.style.filter = "grayscale(1)"
+        item.classList.add("saltar")
+        counter++
+        tableroVca.textContent = counter
+    })
+
+    })
+
+    cerdo.forEach( item => {
+    
+    item.addEventListener("click", () => {
+        item.style.filter = "grayscale(1)"
+        item.classList.add("saltar")
+        counter++
+        tableroCrdo.textContent = counter
     })
 
     })
