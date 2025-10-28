7636da46e9217b268834200a68fdb06de11fdea5
Yuranyy14 <cardenasuribeyurany@gmail.com>
2025-10-28 15:57:31 -0500

    Avance en clase java escenario interactivo

--- 02_Java_escenario_interactivo/app.js
+++ 02_Java_escenario_interactivo/app.js
@@ -77,3 +77,49 @@ insectos.forEach(item => {
     countInsecto.textContent = contadorInsecto;
   });
 });
+
+
+// Carruzel
+
+//1. Variables
+const escenas = document.querySelectorAll(".esc")
+const anterior = document.querySelector("#prev")
+const siguiente = document.querySelector("#next")
+const miniaturas = document.querySelectorAll(".miniatura")
+let indice = 0
+
+console.log(escenas)
+console.log(anterior)
+console.log(siguiente)
+console.log(miniaturas)
+
+//2. Funciones
+function mostrarEscena(i){
+  //Asigna la calse escena a todas las pantallas
+  for (let j = 0; j < escenas.length; j++) {
+    escenas[j].classList.remove("escenarioActivo")
+    
+  }
+
+  escenas[i].classList.add("escenarioActivo")
+
+  indice = i
+}
+//Para mostrar la pantalla 1 al iniciar
+mostrarEscena(0)
+
+anterior.addEventListener("click", function(){
+  indice = indice - 1
+  if(indice < 0){ //Para siguiente if(indice >= escenas.leight)
+    indice = escenas.length - 1 //Va a la ultima imagen
+  }
+  mostrarEscena(indice)
+})
+
+siguiente.addEventListener("click", function(){
+  indice = indice + 1
+  if(indice >= escenas.length){ //Para siguiente if(indice >= escenas.leight)
+    indice = 0 //Va a la ultima imagen
+  }
+  mostrarEscena(indice)
+})   
