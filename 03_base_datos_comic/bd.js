c9947ca122ead64cadfe528c5995da6610e88b37
Yuranyy14 <cardenasuribeyurany@gmail.com>
2025-11-05 17:25:30 -0500

    Base de datos

---
+++ 03_base_datos_comic/bd.js
@@ -0,0 +1,33 @@
+let comic = {
+"nombreComic": "El centurion de la noche",
+"numeroCapitulos": 3,
+"genero": "Biografico", "fantasia", 
+"sinopsis": "Una historia de como Joe se comvirtio en la leyenda que es hoy en dia",
+"autores": "Joseph", "Yurany",
+"portadaComic" : "portada.jpg",
+"year" : 2025,
+"capitulos": [
+    {
+        "id": 1,
+        "nombre": "...",
+        "personajes": ["Joe", "Mujer"],
+        "portada": "portada1.jpg"
+        "escenas": 5,
+        "descripcion": 
+    },
+    {
+        "id": 1,
+        "nombres": "...",
+        "personajes": ["Joe", "Mujer"],
+        "portada": 
+        "descripcion": 
+    },
+    {
+        "id": 1,
+        "nombres": "...",
+        "personajes": ["Joe", "Mujer"],
+        "portada": 
+        "descripcion": 
+    },
+    ]
+}
